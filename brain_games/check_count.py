0411ad7f87f13055bf04ae2ed83a2dc8cd581bb7
LilDrugHill <smookigrow@icloud.com>
2022-03-26 20:39:15 +0400

    Другой вариант

--- brain_games/check_count.py
+++ brain_games/check_count.py
@@ -1,31 +1,17 @@
 """Основание игры."""
 
-from brain_games.expr_result import choosen_expression_result, random_numbers
 from brain_games.scripts.brain_games import main
 
-name = main()
-counter = 0
 
-
-def counter_game(way):
+def counter_game(expression_result, name):
     """Счетчик правильных ответов"""
-    if way == 1:
-        print('Answer "yes" if the number is even, otherwise answer "no".')
-    elif way == 2:
-        print('What is the result of the expression?')
-    elif way == 3:
-        print('Find the greatest common divisor of given numbers.')
-    elif way == 4:
-        print('What number is missing in the progression?')
-    elif way == 5:
-        print('Answer "yes" if given number is prime. Otherwise answer "no".')
-    counter = answer_check(choosen_expression_result(way, random_numbers()), way) 
+    counter = answer_check(expression_result, name) 
     while 3 > counter > 0:          #Функция берет знавения из нижестоящий по логике функции
-        counter += answer_check(choosen_expression_result(way, random_numbers()), way)
+        counter += answer_check(expression_result, name)
     if counter == 3:
         print(f'Congratulations, {name}!')
 
-def answer_check(expression_result, way):
+def answer_check(expression_result, name):
     """Проверка ответа и вывод слов поддержки."""
     print(f'Question: {expression_result[0]}')
     answer = input()
@@ -38,3 +24,15 @@ def answer_check(expression_result, way):
         print(f"Let's try again, {name}!")
         counter =- 10
         return counter
+        
+        
+    """ if way == 1:
+        print('Answer "yes" if the number is even, otherwise answer "no".')
+    elif way == 2:
+        print('What is the result of the expression?')
+    elif way == 3:
+        print('Find the greatest common divisor of given numbers.')
+    elif way == 4:
+        print('What number is missing in the progression?')
+    elif way == 5:
+        print('Answer "yes" if given number is prime. Otherwise answer "no".')"""
