3cb0ed7e67692f64bef67a84f404b03c8c3a6cde
LilDrugHill <101098006+LilDrugHill@users.noreply.github.com>
2022-04-28 19:21:22 +0400

    Update progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -10,14 +10,17 @@ INTRO = 'What number is missing in the progression?'
 def generate_expression_and_answer():
     """Вычисление примера и результата for progression."""
     expression = []
-    rand_len = randrange(RECOMMEND_LEN_MIN, RECOMMEND_LEN_MAX)
-    first_number = randrange(1, MAX_POSSIBLE_VALUE)
-    progression_step = randrange(2, 5)
-    while len(expression) <= rand_len:
-        expression.append(first_number)
-        first_number += progression_step
-    answer = expression[randrange(0, rand_len)]
-    expression[expression.index(answer)] = '..'
+    rand_values_dict = {
+        'rand_len': randrange(RECOMMEND_LEN_MIN, RECOMMEND_LEN_MAX),
+        'first_number': randrange(1, MAX_POSSIBLE_VALUE),
+        'progression_step': randrange(2, 5),
+    }
+    missing_num_index = randrange(0, rand_values_dict['rand_len'])
+    while len(expression) <= rand_values_dict['rand_len']:
+        expression.append(rand_values_dict['first_number'])
+        rand_values_dict['first_number'] += rand_values_dict['progression_step']
+    answer = expression[missing_num_index]
+    expression[missing_num_index] = '..'
     return ' '.join(map(str, expression)), answer
 
 
